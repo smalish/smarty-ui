1fdd598337a8ede7c4ac62a7f938118396a92822
杨颖01 <yangying01@rong360-MacBook422.local>
2020-11-30 14:32:07 +0800

    先关闭eslint

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
-<!--
- * @Author: yangying01
- * @Date: 2020-11-30 11:31:36
- * @LastEditors: yangying01
- * @LastEditTime: 2020-11-30 13:27:03
--->
 <template>
-  <div id="app">
-    smarty-ui
-  </div>
+  <div id="app"> smarty-ui</div>
 </template>
 
 <script>
---
+++ src/component/button.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Author: yangying01
+ * @Date: 2020-11-30 14:15:55
+ * @LastEditors: yangying01
+ * @LastEditTime: 2020-11-30 14:20:10
+-->
+
+<template>
+    <div class="button-view"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .button-view{
+      display: inline-block;
+      line-height: 1;
+      white-space: nowrap;
+      cursor: pointer;
+      background: #fff;
+      border: 1px solid #dcdfe6;
+      color: #606266;
+      -webkit-appearance: none;
+      text-align: center;
+      box-sizing: border-box;
+      outline: none;
+      margin: 0;
+      transition: .1s;
+      font-weight: 500;
+      -moz-user-select: none;
+      -webkit-user-select: none;
+      -ms-user-select: none;
+      padding: 12px 20px;
+      font-size: 14px;
+      border-radius: 4px;
+  }
+</style>
